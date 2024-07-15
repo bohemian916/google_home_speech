82fd8650e9bce89a2b9d1622fd7aa540d0fa72cf
bohemia <bohemian916@gmail.com>
2024-07-15 21:55:41 +0900

    add zunda voice

--- gen_speech.py
+++ gen_speech.py
@@ -1,3 +1,3 @@
 from gtts import gTTS
-tts = gTTS(text='電気消費が4000ワットを超えています。エアコンをきります', lang='ja')
-tts.save('./hello_world_ja.mp3')
+tts = gTTS(text='電気消費が3500ワットを超えています。エアコンをきります', lang='ja')
+tts.save('./alert_3500watt.mp3')
--- sound_test.py
+++ sound_test.py
@@ -1,9 +1,9 @@
 import pychromecast
 
-chromecasts, browser = pychromecast.get_listed_chromecasts(friendly_names=["ベッドルーム"])
+chromecasts, browser = pychromecast.get_listed_chromecasts(friendly_names=["リビングルーム"])
 cast = chromecasts[0]
 mc = cast.media_controller
 cast.wait()
-mc.play_media('https://www.ne.jp/asahi/music/myuu/wave/menuettm.mp3','audio/mp3')
+mc.play_media('https://github.com/bohemian916/google_home_speech/blob/main/notice.mp3?raw=true','audio/mp3')
 #mc.play_media('https://samplelib.com/lib/preview/mp3/sample-3s.mp3','audio/mp3')
 print(mc.status)
